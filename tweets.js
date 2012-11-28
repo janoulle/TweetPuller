2942553b88d974bdc8b03a6c1ff92601f44fd221
Jane Ullah <janeullah@gmail.com>
2012-11-28 16:03:38 -0500

    Added changed files to fix submit and options problems.

--- tweets.js
+++ tweets.js
@@ -23,35 +23,38 @@ var formDiv;
 var notice;
 var info;
 var rst;
-var yesBtn;
-var noBtn;
-    
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('submit').addEventListener('click', submitForm);
-  yesBtn = document.createElement('input');
+var yesBtn = document.createElement("input");
     yesBtn.setAttribute("type","button");
     yesBtn.setAttribute("id","yes");
     yesBtn.setAttribute("value","Yes");
     yesBtn.setAttribute("class","btn btn-primary");
-    yesBtn.addEventListener('click',yesClicked);
-
-    noBtn = document.createElement('input');
+var noBtn = document.createElement('input');
     noBtn.setAttribute("type","button");
     noBtn.setAttribute("id","no");
     noBtn.setAttribute("value","No");
     noBtn.setAttribute("class","btn");
-    noBtn.addEventListener('click',noClicked);
+var noBtn;
+var noClick = false;
+    
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('submit').addEventListener('click', submitForm);
+  yesBtn.addEventListener('click',yesClicked);
+  noBtn.addEventListener('click',noClicked);
 });
 
 
 //Submit the form
 function submitForm(){
     result = document.getElementById('username');
-    username = result.value;
+    if (!username){
+        username = result.value;
+    }
+    console.log(username);
     openConnection();
 }
 
 function clearForm(){
+    username = '';
     rbnExtra = document.querySelectorAll('#resetbtn');
     for(var i = 0; i < rbnExtra.length; ++i) {
         rbnExtra[i].parentNode.removeChild(rbnExtra[i]);
@@ -64,29 +67,33 @@ function clearForm(){
     info.appendChild(noBtn);
     notice.innerHTML = '';
     divTweet.innerHTML = '';
-    formDiv.setAttribute("style","display: none;");
     usr = localStorage["twitter_username"];
+    toggleDivs(usr);
+}
+
+function toggleDivs(status){
     document.getElementsByClassName('control-group')[0].setAttribute("style","display: all;");
-    if (usr){
+    if (status){
         info.setAttribute("style","display: all;");
-        document.getElementById('yes').addEventListener('click',yesClicked);
-        document.getElementById('no').addEventListener('click',noClicked);
+        formDiv.setAttribute("style","display: none;");        
     }
-    else{
-        formDiv.setAttribute("style","display:all");
+    else {
+        info.setAttribute("style","display: none;");
+        formDiv.setAttribute("style","display: all;");
     }
 }
 
 function yesClicked(){
-    username = usr;
+    username = localStorage["twitter_username"];
     info.setAttribute("style","display: none");
-    openConnection();
+    submitForm();
 }
 
 function noClicked(){
-    formDiv.setAttribute("style","display:all");
-    info.setAttribute("style","display: none");
-    openConnection();
+    noClick = true;
+    username = '';
+    info.setAttribute("style","display: none;");
+    formDiv.setAttribute("style","display:all;");
 }
 
 function openConnection(){
@@ -106,6 +113,7 @@ function openConnection(){
 
 function showTweets(){
     //If a successful request was made, proceed.
+    console.log(req.status);
     if (req.status == 200){
         //Hiding the form.
         document.getElementsByClassName('control-group')[0].setAttribute("style","display: none;");
@@ -120,6 +128,7 @@ function showTweets(){
             //Create text nodes
             newContent = document.createTextNode(text['text']);
             date = document.createTextNode(text['created_at']);
+            date.nodeValue  = formatDate(date.nodeValue);
             pwrapper = document.createElement("li");
             breaker = document.createElement("br");
             datewrapper = document.createElement("em");
@@ -136,30 +145,43 @@ function showTweets(){
             //Add the updated div to the body
             document.body.appendChild(tweetdiv);
         }
-        resetButton();
+        showClearButton();
     }
     //Otherwise, show an error message.
     else{
         //There was a problem with the XMLHttprequest
+        var txt;
         var error = document.getElementById('errordiv');
+        //Private account
+        if (req.status == 401){
+            txt = document.createTextNode("text");
+            txt.nodeValue = 'This twitter account appears to be private. Only public tweets can be displayed.';
+            error.appendChild(txt);
+        }
+        //No such user
+        if (req.status == 404){
+            txt = document.createTextNode("text");
+            txt.nodeValue = 'This twitter account does not exist. Please check the username entered.';
+            error.appendChild(txt);
+        }
         error.setAttribute("style","display: all;");
         setTimeout(function() {
             error.setAttribute("style","display: none;");
-        }, 1500);
+            if (txt){txt.parentNode.removeChild(txt);}
+            username = '';
+        }, 2000);
+        
     }
 }
 
-function resetButton(){
+function showClearButton(){
     rbn = document.createElement("input");
     rbn.setAttribute("id","resetbtn");
     rbn.setAttribute("class","btn btn-primary");
     rbn.setAttribute("type","button");
     rbn.setAttribute("value","Clear");
-    rst = document.createElement("div")
-    rst.appendChild(rbn);
-    document.body.appendChild(rst);
-    rbn.addEventListener('click',clearForm);
-    
+    document.body.appendChild(rbn);
+    rbn.addEventListener('click',clearForm);    
 }
 
 //Credit: http://geekswithblogs.net/Nettuce/archive/2010/03/03/javascript-twitter-linkify.aspx
@@ -180,13 +202,66 @@ function Linkify(text) {
     return text;
 }
 
-function link(data){
-    var val = data.nodeValue;
-    var link = document.createElement("a");
-    
-
-}
-
+//Function from: http://www.queness.com/post/8567/create-a-dead-simple-twitter-feed-with-jquery
+function formatDate(dateString) {
+        var rightNow = new Date();
+        var then = new Date(dateString);
+         
+        if ($.browser.msie) {
+            // IE can't parse these crazy Ruby dates
+            then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
+        }
+ 
+        var diff = rightNow - then;
+ 
+        var second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        week = day * 7;
+        
+        if (isNaN(diff) || diff < 0) {
+            return ""; // return blank string if unknown
+        }
+ 
+        if (diff < second * 2) {
+            // within 2 seconds
+            return "right now";
+        }
+ 
+        if (diff < minute) {
+            return Math.floor(diff / second) + " seconds ago";
+        }
+ 
+        if (diff < minute * 2) {
+            return "about 1 minute ago";
+        }
+ 
+        if (diff < hour) {
+            return Math.floor(diff / minute) + " minutes ago";
+        }
+ 
+        if (diff < hour * 2) {
+            return "about 1 hour ago";
+        }
+ 
+        if (diff < day) {
+            return  Math.floor(diff / hour) + " hours ago";
+        }
+ 
+        if (diff > day && diff < day * 2) {
+            return "yesterday";
+        }
+ 
+        if (diff < day * 365) {
+            return Math.floor(diff / day) + " days ago";
+        }
+        
+        else {
+            return "over a year ago";
+        }
+    } // timeAgo()
+  
 
 /* Google Analytics Tracking*/
 var _gaq = _gaq || [];
