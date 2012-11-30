e55c087b889e0562cd7cb4efbeea00797c0cdf32
Jane Ullah <janeullah@gmail.com>
2012-11-30 03:24:41 -0500

    Added extra option to options.html for specifying number of tweets to retrieve.

--- options.js
+++ options.js
@@ -3,9 +3,17 @@
 // Saves options to localStorage.
 function save_options() {
   var input = document.getElementById('username');
+  var tweetsize = document.getElementById('tweetsize');
   var username = input.value;
-  console.log(username);
+  var sizeval = tweetsize.value;
   localStorage["twitter_username"] = username;
+  if (sizeval > 0 && sizeval <= 40){
+    localStorage["tweet_size"] = sizeval;
+  }
+  else{
+    localStorage["tweet_size"] = 10;
+    alert("Defaulting to tweet size of 10 due to invalid value entered.");
+  }
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
@@ -18,15 +26,18 @@ function save_options() {
 // Restores select box state to saved value from localStorage.
 function restore_options() {
   var username = localStorage["twitter_username"];
-  if (!username){
+  var size = localStorage["tweet_size"];
+  if (!username || !size){
     return;
   }
   localStorage["twitter_username"] = username;
+  localStorage["tweet_size"] = tweetsize;
 }
 
 function erase_options(){
     localStorage.removeItem("twitter_username");
-    status.innerHTML = "Stored twitter name removed.";
+    localStorage.removeItem("tweet_size");
+    status.innerHTML = "Stored options removed.";
     setTimeout(function() {
         status.innerHTML = "";
     }, 1500);
